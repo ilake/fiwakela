0ec0db4be578278dd7f56b631dad63aa4ab861f7
ilake <lake.ilakela+github@gmail.com>
2010-01-13 21:42:19 +0800

    增加 rank 現在情況的描述

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -3,7 +3,7 @@ class RanksController < ApplicationController
   layout 'records'
   def index
     @timezone = params[:timezone] ? params[:timezone] : @user.timezone
-    @hour     = params[:date] ? params[:date][:hour] : 8
+    @hour     = params[:date] ? params[:date][:hour] : 7
 
     Time.zone = @timezone 
     @target_time = Time.zone.local(1982, 1, 8, @hour, 0, 0)
