e6c25a77119785ad3d8966210012400a3f064a0f
ilake <lake.ilakela+github@gmail.com>
2010-01-28 14:38:21 +0800

    正常吧

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,9 +68,9 @@ namespace :fiwakela do
   end
 end
 
-
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
-require 'hoptoad_notifier/capistrano'
+#
+#Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+#  $: << File.join(vendored_notifier, 'lib')
+#end
+#
+#require 'hoptoad_notifier/capistrano'
