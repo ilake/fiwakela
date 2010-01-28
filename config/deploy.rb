31bdf1890cd2293df718737b102574daabe50746
ilake <lake.ilakela+github@gmail.com>
2010-01-28 12:27:55 +0800

    Change hoptoad plugin to gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,3 +67,10 @@ namespace :fiwakela do
     start
   end
 end
+
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
