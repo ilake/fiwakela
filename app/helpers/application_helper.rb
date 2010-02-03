0aab6e44fb46a5438aa48e47a0dd78bd63a42c6a
ilake <lake.ilakela+github@gmail.com>
2010-02-03 14:56:38 +0800

    避開user 沒image 的狀況

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def user_image_tag(img = 'http://static.ak.fbcdn.net/pics/q_silhouette.gif')
-    image_tag(img)
+  def user_image_tag(img)
+    image_tag(img || 'http://static.ak.fbcdn.net/pics/q_silhouette.gif')
   end
 end
